dff55fe61e96565cd9ba364bc799ed1a8be31780
anudeepika.a <anudeepika.a@releaseowl.com>
2026-03-30 11:18:41 +0530

    Initial ESLint Sonar project

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,25 @@
 'use strict'
 
 const eslint_js = require('@eslint/js')
-const tseslint = require('typescript-eslint');
 const globals = require('globals')
 
 module.exports = [
   {
-    ignores: ["**/dist/*", "gen/**/*", "@cds-models/**/*", "app/**/*"],
+    ignores: ["**/dist/*", "gen/**/*", "node_modules/**/*"],
   },
-  // global rules for all files
   eslint_js.configs.recommended,
-  tseslint.configs.base,
-  // Generic config for JavaScript files: Setup environment, version, etc.
   {
-    files: ["**/*.js", "**/*.ts"],
+    files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
       globals: {
         ...globals.node,
-        ...globals.jest,
         ...globals.es6,
-        SELECT: true,
-        INSERT: true,
-        UPDATE: true,
-        DELETE: true,
-        CREATE: true,
-        DROP: true,
-        CDL: true,
-        CQL: true,
-        CXL: true,
-        cds: true,
-        sap: true,
       },
     },
     rules: {
-      "no-console": "off",
-      "require-atomic-updates": "off",
+      "no-console": "off"
     },
   },
 ];
